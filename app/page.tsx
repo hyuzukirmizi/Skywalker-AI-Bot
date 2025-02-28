c6fca3896e5bc4cf31a67cf0710a78c841a110ef
HYuzukirmizi <hyuzukirmizi@umass.edu>
2025-02-27 19:34:29 -0500

    publish

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,133 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from 'react'
+import { message, type Message } from './actions'
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inputMessage, setInputMessage] = useState("")
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      role: "system",
+      content: "You are a helpful assistant and you'll be answering questions to math as famous fictional character Anakin Skywalker"
+    },
+  ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function sendMessage() {
+
+    setIsLoading(true)
+
+    const messageHistory = [
+      ...messages,
+      {
+        role: "user",
+        content: inputMessage
+      },
+    ];
+
+    const response = await message(messageHistory);
+
+
+    if (response) {
+      messageHistory.push(response);
+    }
+
+    setMessages(messageHistory);
+    setIsLoading(false)
+    setInputMessage("")
+  }
+
+
+
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <header className="bg-white p-2">
+        <div className="flex lg:flex-1 items-center justify-center">
+          <a href="#" className="m-1.5">
+            <span className="sr-only">skywalker Chat application</span>
+            <img
+              className="h-8 w-auto"
+              src="http://localhost:3000/skywalker.svg"
+              alt=""
             />
-            Deploy now
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <h1 className="text-black font-bold">Chat application</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </header>
+
+      <div className="flex flex-col h-full">
+        {messages && messages.length > 0 && messages.map(({ role, content }, index) => {
+
+          if (role === "user") {
+            return (
+              <div key={role + index} className="col-start-1 col-end-8 p-3 rounded-lg">
+                <div className="flex flex-row items-center">
+                  <div
+                    className="flex items-center justify-center h-8 w-8 rounded-full bg-orange-400 text-white flex-shrink-0 text-sm"
+                  >
+                    Me
+                  </div>
+                  <div
+                    className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                  >
+                    <div>{content}</div>
+                  </div>
+                </div>
+              </div>
+            );
+          }
+
+          if (role === "assistant") {
+
+            return (
+              <div key={role + index} className="col-start-6 col-end-13 p-3 rounded-lg">
+                <div className="flex items-center justify-start flex-row-reverse">
+                  <div
+                    className="flex items-center justify-center h-8 w-8 rounded-full bg-green-400 flex-shrink-0 text-sm"
+                  >
+                    AI
+                  </div>
+                  <div
+                    className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                  >
+                    <div>{content}</div>
+                  </div>
+                </div>
+              </div>
+            );
+          }
+
+        })}
+      </div>
+
+      <div className="flex flex-col flex-auto justify-between bg-gray-100 p-6">
+        <div
+          className="top-[100vh] flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className="flex-grow ml-4">
+            <div className="relative w-full">
+              <input
+                type="text"
+                disabled={isLoading}
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+                className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+              />
+            </div>
+          </div>
+          <div className="ml-4">
+            <button
+              onClick={sendMessage}
+              className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-2 flex-shrink-0"
+            >
+              <span>{isLoading ? "Loading..." : "Send"}</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
